2a8860b8a7d2fca915142e7b411ee597fb9acefa
rizwanmetax25 <rizwan@metaxpayments.com>
2026-06-08 19:27:49 +0400

    updates

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,27 @@ export * from "./practiceAreas";
 export * from "./testimonials";
 export * from "./faqs";
 export * from "./caseStudies";
+
+
+// <img
+//   id="hero-image"
+//   src="/images/lawyer-1.jpg"
+//   alt="Lawyer"
+// />
+
+// <script>
+//   const images = [
+//     "/images/lawyer-1.jpg",
+//     "/images/lawyer-2.jpg",
+//     "/images/lawyer-3.jpg",
+//     "/images/lawyer-4.jpg",
+//   ];
+
+//   let current = 0;
+//   const image = document.getElementById("hero-image");
+
+//   setInterval(() => {
+//     current = (current + 1) % images.length;
+//     image.src = images[current];
+//   }, 3000);
+// </script>
